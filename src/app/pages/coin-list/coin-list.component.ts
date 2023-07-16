6cc7274d35c2c4abf86fa587a331e9f84786c2fa
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-07-16 17:22:19 -0300

    style: added title and styles of list of currency

--- src/app/pages/coin-list/coin-list.component.ts
+++ src/app/pages/coin-list/coin-list.component.ts
@@ -39,3 +39,6 @@ export class CoinListComponent implements OnInit {
 }
 
       // 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Cethereum%2Clitecoin%2Ccardano%2Cdogecoin&vs_currencies=usd&include_24hr_change=true';
+
+
+     // https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Cethereum%2Clitecoin%2Ccardano%2Cdogecoin&vs_currencies=usd&include_24hr_change=true
